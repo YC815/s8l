77f5979f4eabe9a6dfac0fd4ef64cc4e1f98a15c
YC815 <yushun@chen.zone>
2025-07-17 23:24:36 +0800

    feat(ads): 新增 Google 廣告腳本至佈局

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-TW" className="dark:bg-gray-900">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9993759051139856"
+             crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
